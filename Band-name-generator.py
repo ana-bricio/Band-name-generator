b1ffe6ac02e221cf19244f4afd7ff74aa03876b0
Ana Bricio <ana.bricio98@gmail.com>
2024-05-27 14:02:29 -0600

    Add files via upload

---
+++ Band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+color = input("What's your favorite color?\n")
+lunch = input("What did you have for lunch?\n")
+
+print("Your band name could be " + color + " " + lunch)
